93bc41d303dc2f699da982c3da91501c5c0f8366
IIMRguy <55660612+IIMRguy@users.noreply.github.com>
2026-09-19 20:28:28 +0530

    Update TERM5_CLOUD_URL to production URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.TERM5_CLOUD_URL = 'PASTE_WEB_APP_URL_HERE';
+window.TERM5_CLOUD_URL = 'https://script.google.com/macros/s/AKfycbyExdF-fs5aCERB5GepQNWa2PrudLaXKnqK55sRvHD3quh-naUPCpMT-H_I2rPZWgx4/exec';
